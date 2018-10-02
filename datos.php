0f8fd87c10832c477a2d8f764efd0800cd7e44e3
tahal32 <41704356+tahal32@users.noreply.github.com>
2018-10-02 00:29:20 -0600

    Add files via upload

--- datos.php
+++ datos.php
@@ -1,18 +1,7 @@
 <?php
 
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$edad = $_POST["edad"];
 
-
-header('Content-Type: application/json');
-//Guardamos los datos en un array
-$datos = array(
-'estado' => 'ok',
-'nombre' => $nombre, 
-'apellido' => $apellido, 
-'edad' => $edad
-);
-
-echo json_encode($datos, JSON_FORCE_OBJECT);
-?>
+   echo "<?xml version='1.0'?>";
+  echo "<nombre>", $_POST['nombre'] ,"</nombre>" ;
+  echo "<apellido>" ,$_POST['apellido'] ,"</apellido>";
+ ?>
